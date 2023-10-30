a244b93faa6ee8828372966a1315e8c5db3afc09
incyberz <incyberz@yahoo.com>
2023-10-30 14:45:42 +0700

    update reset password

---
+++ pages/reset_password_confirm.php
@@ -0,0 +1,24 @@
+<?php
+$tanggal=  date('D, M d, Y, H:i:s');
+$link_encoded = urlencode("https://pesantrenalmasoem.id/dipa/?verifikasi_wa_for_instruktur&username=$username&kelas=$kelas&no_wa=$no_wa");
+$text_wa = "*REQUEST RESET PASSWORD*%0a%0aYt. Instruktur DIPA Joiner,%0aSaya $nama, mengajukan Request Reset Password atas username: *$username* kelas *$kelas* dengan nomor-wa-aktif: *$no_wa*. Mohon untuk segera diverifikasi. Terimakasih.%0a%0aLink untuk Instruktur:%0a$link_encoded %0a%0a [DIPA Joiner Apps, $tanggal]
+";
+$text_wa_show = str_replace('%0a',' ',$text_wa);
+?>
+<section id="team" class="team section-bg">
+  <div class="container">
+
+    <div class="section-title" data-aos="fade-up">
+      <h2>Reset</h2>
+      <p>Setelah Reset Password maka password akan sama dengan username</p>
+    </div>
+    <div class="wadah gradasi-hijau" data-aos="fade-up" data-aos-delay="150">
+      <div class="form-group">
+        <textarea class="form-control" rows="10" disabled><?=$text_wa_show?></textarea>
+      </div>
+      <div class="form-group">
+        <a href='https://api.whatsapp.com/send?phone=6287729007318&text=<?=$text_wa?>' class="btn btn-primary btn-block">Kirim Pesan Reset Password</a>
+      </div>
+    </div>
+  </div>
+</section>
--- pages/verifikasi_wa.php
+++ pages/verifikasi_wa.php
@@ -1,22 +1,106 @@
+<?php
+$debug = '';
+$hide_form = 0;
+$hide_section = 0;
+$form = '';
+$dari = '';
+$untuk = $_GET['untuk'] ?? '';
+if(!$is_login AND $untuk=='') die('<script>location.replace("?")</script>');
+$pesan = 'Untuk mengakses semua fitur kamu harus update dengan nomor whatsapp yang aktif.';
+
+$caption = 'Verifikasi';
+if($id_peserta==''){ // belum login
+  $debug.= "
+  <span class=debug><span id=is_login>is_login:$is_login</span></span>
+  ";
+
+  if(isset($_POST['username'])){
+    $username = $_POST['username'];
+    $kelas = $_POST['kelas'];
+    $dari = $_POST['dari'];
+
+    if($dari=='reset_password') $caption = 'Reset Password';
+
+    $pesan = "Request reset password dari username: <b class=darkblue>$username</b> kelas <b class=darkblue>$kelas</b>";
+    $s = "SELECT no_wa,nama FROM tb_peserta WHERE username='$username' AND kelas='$kelas'";
+    $q = mysqli_query($cn,$s) or die(mysqli_error($cn));
+    if(mysqli_num_rows($q)==0){
+      $link_back = $dari=='' ? '' : " | <a href='?$dari'>Kembali</a>";
+      $pesan.= div_alert('danger', "Maaf, data untuk username $username kelas $kelas tidak ditemukan. $link_back");
+      $hide_form = 1;
+    }else{
+      $d=mysqli_fetch_assoc($q);
+      $no_wa = $d['no_wa'];
+      $nama = $d['nama'];
+
+      if($no_wa==''){
+        $pesan.= div_alert('info',"Anda belum memasukan Nomor Whatsapp. Silahkan masukan dahulu nomor yang aktif.");
+      }else{
+        $pesan.= div_alert('info',"Data whatsapp sudah ada, silahkan klik Verifikasi/Reset. Jika nomor aktif saat ini berbeda maka silahkan input kembali.");
+      }
+
+      
+
+    }    
+  }elseif(isset($_POST['btn_submit'])){
+    $dari = $_POST['dari'];
+
+    if($_POST['dari']=='reset_password'){
+
+      $username = $_POST['username_reset'];
+      $kelas = $_POST['kelas'];
+      $no_wa = $_POST['no_wa'];
+      $nama = $_POST['nama'];
+      include 'reset_password_confirm.php';
+      $hide_section=1;
+      
+    }else{
+      // verifikasi normal
+      echo "VERIFIKASI NORMAL";
+    }
+  }else{
+    $pesan.= div_alert('danger','No POST request detected.');
+    exit;
+  }
+
+
+}
+
+if(!$hide_form){
+  $form = "
+  <form method=post>
+    <input class=debug name=nama value='$nama'>
+    <input class=debug name=dari value='$dari'>
+    <input class=debug name=username_reset value='$username'>
+    <input class=debug name=kelas value='$kelas'>
+    <div class='wadah gradasi-hijau' data-aos='fade-up' data-aos-delay='200'>
+      <label for='no_wa' class='tengah mb1'>Nomor WhatsApp <span class='kecil abu miring'>* yang aktif</span></label>
+      <input type='text' class='form-control tengah' minlength=11 maxlength=14 id=no_wa autocomplete=off style='color:gray' name=no_wa required>
+      <div class='tengah consolas' style='font-size:30px' id=no_wa2>628X-XXX-XXX-XXX</div>
+      <div class='tengah consolas red' style='font-size:10px' id=no_wa_invalid>awali dg '08...' atau '62...'</div>
+      <div>
+        <button class='btn btn-primary btn-block' id=btn_verifikasi name=btn_submit>$caption</button>
+      </div>
+    </div>
+  </form>
+  ";
+}
+
+if(!$hide_section){
+?>
 <section>
   <div class="container">
 
     <div class="section-title" data-aos="fade-up">
       <h2>Verifikasi WA</h2>
-      <p>Untuk mengakses semua fitur kamu harus update dengan nomor whatsapp yang aktif.</p>
+      <p><?=$pesan?></p>
     </div>
-
-    <div class="wadah gradasi-hijau" data-aos='fade-up' data-aos-delay='200'>
-      <label for="no_wa" class='tengah mb1'>Nomor WhatsApp <span class='kecil abu miring'>* yang aktif</span></label>
-      <input type="text" class="form-control tengah" maxlength=14 id=no_wa autocomplete=off style='color:gray'>
-      <div class="tengah consolas" style='font-size:30px' id=no_wa2>628X-XXX-XXX-XXX</div>
-      <div class="tengah consolas red" style='font-size:10px' id=no_wa_invalid>awali dg "08..." atau "62..."</div>
-      <div>
-        <a class="btn btn-primary btn-block" id=btn_verifikasi>Verifikasi</a>
-      </div>
+      <?=$form?>
+      <?=$debug?>
     </div>
   </div>
 </section>
+<?php } ?>
 
 
 <script>
